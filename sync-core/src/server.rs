5947a1b67142ecbe01d516944187610709ed90aa
zhengpeng <847850277@qq.com>
2024-11-02 22:59:04 +0800

    [ISSUES#37] feat: sync example part 01

---
+++ sync-core/src/server.rs
@@ -0,0 +1,19 @@
+use std::net::{TcpListener, TcpStream};
+
+pub struct Server{
+
+}
+
+
+
+impl Server {
+    pub fn new() -> Self {
+        Self{}
+    }
+
+    pub fn start(&self) {
+        println!("todo start tcp server...");
+    }
+}
+
+
---
+++ sync-examples/src/main.rs
@@ -0,0 +1,6 @@
+use sync_core::server::Server;
+fn main() {
+    let mut server = Server::new();
+    server.start();
+
+}
