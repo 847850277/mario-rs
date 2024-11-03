184bf26f31f1d1e37a6b44b6368677205c47bdfb
zhengpeng <847850277@qq.com>
2024-11-03 19:42:23 +0800

    [ISSUES#37] feat: sync example part 02 run server

--- sync-core/src/server.rs
+++ sync-core/src/server.rs
@@ -1,19 +1,41 @@
+use log::info;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-pub struct Server{
-
-}
-
-
+pub struct Server {}
 
 impl Server {
     pub fn new() -> Self {
-        Self{}
+        Self {}
     }
 
     pub fn start(&self) {
-        println!("todo start tcp server...");
+        //println!("todo start tcp server...");
+        let addr = "127.0.0.1:8080";
+        // start tcp server
+        let listener = TcpListener::bind(addr).unwrap();
+        info!("Listening on http://{}", addr);
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    self.handle_connection(stream);
+                }
+                Err(e) => {
+                    eprintln!("failed: {}", e);
+                }
+            }
+        }
+    }
+
+    fn handle_connection(&self, mut tcp_stream: TcpStream) {
+        //println!("todo handle connection...");
+        let mut buffer = [0; 1024];
+        tcp_stream.read(&mut buffer).unwrap();
+        println!("Request: {}", String::from_utf8_lossy(&buffer));
+
+        // response
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        let response = format!("{}{}", response, "Hello World");
+        tcp_stream.write(response.as_bytes()).unwrap();
     }
 }
-
-
--- sync-examples/src/main.rs
+++ sync-examples/src/main.rs
@@ -1,6 +1,7 @@
 use sync_core::server::Server;
 fn main() {
+    //trace log
+    tracing_subscriber::fmt::init();
     let mut server = Server::new();
     server.start();
-
 }
