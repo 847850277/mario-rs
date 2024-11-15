a865014c0f07e8e90ff3b80597d64363d0776947
zhengpeng <847850277@qq.com>
2024-11-15 21:31:10 +0800

    [ISSUES#58] POST && PUT handler

--- axum-like-examples/src/main.rs
+++ axum-like-examples/src/main.rs
@@ -1,12 +1,17 @@
 use std::future::Future;
 use std::net::SocketAddr;
 
-use axum_like::{handler::get, Router};
+use axum_like::{handler::get,handler::post, Router};
+use axum_like::handler::put;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/post",post(post_handler))
+        .route("/put",put(put_handler))
+        ;
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -17,9 +22,13 @@ async fn main() {
         .unwrap();
 }
 
-
-
-
 async fn handler() -> &'static str {
     "<h1>Hello, World!</h1>"
+}
+async fn post_handler() -> &'static str {
+    "<h1> Post Hello, World!</h1>"
+}
+
+async fn put_handler() -> &'static str {
+    "<h1> Put Hello, World!</h1>"
 }
