0b3ff3c62e3db83d90187a7c642bec911d366ac3
zhengpeng <847850277@qq.com>
2024-11-22 21:57:56 +0800

    [ISSUES#78] extra param

--- mario-rs-examples/src/main.rs
+++ mario-rs-examples/src/main.rs
@@ -1,16 +1,17 @@
-use std::future::Future;
-use std::pin::Pin;
-use std::sync::Arc;
-
+use http_body_util::Full;
 use log::info;
-
 use mario_core::error::Error;
+use mario_core::extra::{FailedToDeserializeQueryString, Query};
 use mario_core::handler::Endpoint;
 use mario_core::response::Response;
 use mario_core::route::Router;
 use mario_core::server::Server;
 use mario_core::service::Service;
 use mario_macro::handler;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
+use warp::hyper::body::Bytes;
 
 async fn example() -> Response<String> {
     Response::new("run example".to_string())
@@ -18,8 +19,8 @@ async fn example() -> Response<String> {
 fn example_1() -> String {
     "run example_1".to_string()
 }
-fn example_2(i: i32) -> String {
-    let string = format!("run example_2: {}", i);
+fn example_2<T: std::fmt::Display>(param: Query<T>) -> String {
+    let string = format!("run example_2: {}", param.0);
     println!("{}", string);
     string
 }
@@ -49,11 +50,19 @@ pub struct ExtraExample;
 
 impl Endpoint for ExtraExample {
     fn call(&self, req: &mario_core::request::Request) -> Result<Response<String>, Error> {
-        let copy_req = Arc::new(req.clone());
+        let copy_req = Arc::new(req);
         let query = copy_req.head.uri.query().unwrap_or_default();
         info!("query: {:?}", query);
-        let response = example_2(1);
-        Ok(Response::new(response.to_string()))
+        let result = serde_urlencoded::from_str::<Vec<(String, String)>>(query);
+        let result = match result {
+            Ok(result) => result,
+            Err(_) => return Ok(Response::new("500 Internal Server Error".to_string())),
+        };
+        if (result.len() > 0) {
+            let response = example_2(Query(result[0].1.clone()));
+            return Ok(Response::new(response.to_string()));
+        }
+        Ok(Response::new("not param".to_string()))
     }
 }
 
